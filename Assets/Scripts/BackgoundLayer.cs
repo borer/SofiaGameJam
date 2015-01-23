ce1e0a2d5293f7f279186f0f87adc2be99124e41
HypnoBeaverMoose <y.atanasoff@gmail.com>
2015-01-24 01:24:36 +0200

    added demo scene, and background generation

---
+++ Assets/Scripts/BackgoundLayer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class BackgoundLayer : MonoBehaviour {
+
+
+    public GameObject m_prefab;
+    private List<GameObject> m_planes = new List<GameObject>();
+    private const float MARGIN = 0.2f;
+    public Material[] m_skyColours;
+    public Material[] m_transitions;
+    public float[] m_heights;
+    private int m_heightIndex = 0;
+    private float m_offset = 0;
+	void Start () 
+    {
+        var go = (GameObject)Instantiate(m_prefab, transform.position, Quaternion.identity);
+        float maxY = float.MinValue;
+        float minY = float.MaxValue;
+        for (int i = 0; i < go.GetComponent<MeshFilter>().mesh.vertexCount; i++)
+        {
+            if (go.GetComponent<MeshFilter>().mesh.vertices[i].y > maxY)
+                maxY = go.GetComponent<MeshFilter>().mesh.vertices[i].y;
+            if (go.GetComponent<MeshFilter>().mesh.vertices[i].y < minY)
+                minY = go.GetComponent<MeshFilter>().mesh.vertices[i].y;
+        }
+
+        m_offset = (maxY - minY) * go.transform.localScale.y;
+        Debug.Log(minY);
+        Debug.Log(maxY);
+
+        m_planes.Add(go);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 
+        var last = m_planes[m_planes.Count - 1];
+
+        float size = last.transform.localScale.y;
+
+        if (last.transform.position.y + (size * MARGIN) / 2 < Camera.main.GetComponent<CameraMover>().TopLimit(0))
+        {
+            Vector3 offset = new Vector3(0, m_offset, 0);
+
+            var go = (GameObject)Instantiate(m_prefab, last.transform.position + offset, Quaternion.identity);
+            if (m_heightIndex < m_heights.Length && go.transform.position.y > m_heights[m_heightIndex])
+                go.renderer.material = m_transitions[m_heightIndex++];  
+            else
+                go.renderer.material = m_skyColours[m_heightIndex];
+
+
+            m_planes.Add(go);            
+            if (m_planes.Count > 4)
+            {
+                Destroy(m_planes[0]);
+                m_planes.RemoveAt(0);
+            }
+
+        }
+	}
+}
---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+    public float m_speed = 1;
+	public const float LIMIT = 2;
+    
+    void Start () 
+    {
+
+        //for (int i = 0; i < 1000; i+=10)
+        //{
+        //    Vector3 pos = new Vector3(0, 0, i);
+        //    Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), new Vector3(0, TopLimit(i), i), Quaternion.identity);
+        //    Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), new Vector3(0, BottomLimit(i), i), Quaternion.identity);
+        //    Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), new Vector3(LeftLimit(i), 0, i), Quaternion.identity);
+        //    Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube), new Vector3(RightLimit(i), 0, i), Quaternion.identity);
+        //}
+	
+	}
+
+    void Update() 
+    {
+
+        var pos = transform.position;
+        pos.y += m_speed * Time.deltaTime;
+        transform.position = pos;
+	}
+
+    public float TopLimit(float distance)
+    {
+        return transform.position.y + LIMIT;
+        //return transform.position.y + Mathf.Tan(camera camera.fieldOfView / 2 ) * distance;
+    }
+
+    public float BottomLimit(float distance)
+    {
+        return transform.position.y - LIMIT;
+        //return transform.position.y - Mathf.Tan(camera.fieldOfView / 2) * distance;
+    }
+
+    public float LeftLimit(float distance)
+    {
+        return transform.position.x - LIMIT;
+        //return transform.position.x - Mathf.Tan((camera.aspect * camera.fieldOfView) / 2) * distance;
+    }
+
+    public float RightLimit(float distance)
+    {
+        return transform.position.x + LIMIT;
+        //return transform.position.x + Mathf.Tan((camera.aspect * camera.fieldOfView) / 2) * distance;
+    }
+
+}
