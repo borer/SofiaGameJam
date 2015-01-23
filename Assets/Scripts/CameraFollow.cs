d698bf3ebc0dcc4511e7602c940acdd73761cc70
Bogdan Gochev <bogdan.dgochev@gmail.com>
2015-01-23 23:35:55 +0200

    added basic player movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Vector3 distanceFromObject = new Vector3(0, 1, -1);
+	public Transform objectToFollow;
+	
+	void LateUpdate () {
+		transform.position = objectToFollow.position + distanceFromObject;
+	}
+}
