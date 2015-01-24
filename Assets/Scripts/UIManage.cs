99d79ab77dc773e87c962f838541678b3cff4adc
Vladislav <vladislav.varadinov@gmail.com>
2015-01-24 19:08:55 +0200

    UI

---
+++ Assets/Scripts/UIManage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManage : MonoBehaviour {
+	  
+
+		
+		public Animator startButton;
+		public Animator exitButton;
+		
+		public void OpenSettings()
+		{
+			startButton.SetBool("isHidden", true);
+			exitButton.SetBool("isHidden",true);
+		}
+	}
