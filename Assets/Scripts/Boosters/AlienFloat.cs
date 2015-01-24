c0d419aa99d1502778d0bc54f88a81363b0b60e3
HypnoBeaverMoose <y.atanasoff@gmail.com>
2015-01-24 14:51:58 +0200

    added some more assets

---
+++ Assets/Scripts/Boosters/AlienFloat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienFloat : MonoBehaviour {
+
+    public float m_radius = 1;
+    public float m_speed = 10;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offsetX = Mathf.Sin(Time.time * m_speed) * m_radius;
+        float offsetY = Mathf.Cos(Time.time * m_speed) * m_radius;
+        transform.position += new Vector3(offsetX, offsetY, 0);
+	}
+}
---
+++ Assets/Scripts/Boosters/BottleRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottleRotate : MonoBehaviour {
+
+    public float m_speed = 10;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation *= Quaternion.AngleAxis(m_speed * Time.smoothDeltaTime, Vector3.right);
+	}
+}
