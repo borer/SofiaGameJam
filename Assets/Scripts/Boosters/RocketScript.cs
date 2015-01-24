ebbdfaa93695edbe0642d0860535c1e3db26544a
HypnoBeaverMoose <y.atanasoff@gmail.com>
2015-01-24 20:34:07 +0200

    added scripts for rocket and plane

---
+++ Assets/Scripts/Boosters/RocketScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketScript : MonoBehaviour {
+
+
+    private float m_speed;
+	void Start () {
+        m_speed = Random.Range(1, 10);
+	
+	}
+	
+
+    void Update () {
+        transform.Translate(0, m_speed * Time.smoothDeltaTime, 0);
+	}
+}
