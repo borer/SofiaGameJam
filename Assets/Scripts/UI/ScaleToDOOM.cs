629c4b5fc9aa5d9f5af3fca1db0073d83816167f
Bogdan Gochev <bogdan.dgochev@gmail.com>
2015-01-25 04:18:46 +0200

    added scale ui and score ui

--- Assets/Scripts/UI/ScaleToDOOM.cs
+++ Assets/Scripts/UI/ScaleToDOOM.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class ScaleToDOOM : MonoBehaviour {
 
-	public RectTransform scaleIndicator;
+	public RectTransform scaleOstrichIndicator;
+	public RectTransform scaleAsteroidIndicator;
 	public Transform asteroid;
 	public Transform player;
 
-	private float maxHeight;
+	private float targetHeight;
+	private float scaleMinHeight;
+	private float scaleWidth;
 
 	void Start() {
-		maxHeight = asteroid.position.y;
+		targetHeight = asteroid.position.y;
+		scaleMinHeight = scaleOstrichIndicator.anchoredPosition.y;
+		scaleWidth = scaleOstrichIndicator.anchoredPosition.y - scaleAsteroidIndicator.anchoredPosition.y;
 	}
 
 	void LateUpdate () {
-		var percent =  (player.position.y / maxHeight) * 100;
+		var progress = (player.position.y / targetHeight);
+		var oldPosition = scaleOstrichIndicator.anchoredPosition;
+		scaleOstrichIndicator.anchoredPosition = new Vector2(oldPosition.x, scaleMinHeight - progress * scaleWidth );
 	}
 }
