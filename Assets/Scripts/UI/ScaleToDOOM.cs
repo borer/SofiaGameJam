5d6cc6110e1dca2d6438cb508d38723700935bf0
Bogdan Gochev <bogdan.dgochev@gmail.com>
2015-01-25 02:08:35 +0200

    Started implemented scale

---
+++ Assets/Scripts/UI/ScaleToDOOM.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleToDOOM : MonoBehaviour {
+
+	public RectTransform scaleIndicator;
+	public Transform asteroid;
+	public Transform player;
+
+	private float maxHeight;
+
+	void Start() {
+		maxHeight = asteroid.position.y;
+	}
+
+	void LateUpdate () {
+		var percent =  (player.position.y / maxHeight) * 100;
+	}
+}
